88e19af477f1bf88416c2d17b7523cd2b83f9e29
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-30 19:31:43 -0700

    Add flights from and to airport to database

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,20 +1,5 @@
 class FlightsController < ApplicationController
-    def new
-        @flight = Flight.new
-    end
-
     def index
-        @flights = Flight.all
-    end
-
-    def create
-        @flight = Flight.new(flight_params)
-    
-        if @flight.save
-          flash[:notice] = 'Flight has been created.'
-          redirect_to @flight
-        else
-          render :new
-        end
+      @flights = Flight.all
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Airport.destroy_all
+Flight.destroy_all
 
+@airports = Airport.create([{ airport_code: 'SFO' },
+                { airport_code: 'JFK'},
+                { airport_code: 'SEA'},
+                { airport_code: 'PDX'},
+                { airport_code: 'DEN'},
+                { airport_code: 'ATL'}])
 
-airports = Airport.create([{ airport_code: 'San Francisco International Airport (SFO)' },
-                           { airport_code: 'John F. Kennedy International Airport (JFK)'},
-                           { airport_code: 'Seattle–Tacoma International Airport (SEA)'},
-                           { airport_code: 'Portland International Airport (PDX)'},
-                           { airport_code: 'Denver International Airport (DEN)'},
-                           { airport_code: 'Hartsfield-Jackson Atlanta International Airport (ATL)'}])
+Flight.create!([{ from_airport_id: @airports.slice(0).id,
+                 to_airport_id: @airports.slice(1).id,
+                flight_date: 2021-02-02,
+                duration: '5 hours and 47 minutes'
+                },
 
+                { from_airport_id: @airports.slice(2).id,
+                  to_airport_id: @airports.slice(3).id,
+                 flight_date: 2021-05-11,
+                 duration: '2 hours and 20 minutes'
+                },
+
+                { from_airport_id: @airports.slice(4).id,
+                  to_airport_id: @airports.slice(5).id,
+                 flight_date: 2021-01-06,
+                 duration: '4 hours and 6 minutes'
+                }
+                ])
+
+p "Created #{Airport.count} airport entries and #{Flight.count} flight entries."
