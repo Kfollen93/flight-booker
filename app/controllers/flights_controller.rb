040bf5f3f685ae685e38cd2d8e92bb65b2c2490e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-02 16:54:33 -0700

    Move search form into partial for flights index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# Holds actions regarding viewing airports and flights
 class FlightsController < ApplicationController
-    def index
-      @airports = Airport.all.first
-      @flights = Flight.all
-    end
+  def index
+    @airports = Airport.all.first
+    @flights = Flight.all
+  end
 end
