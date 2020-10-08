fee9855e2be719fd13f6ad66370294612d310718
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-08 11:21:44 -0700

    Minor formatting for readability

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,9 +3,11 @@
 # Holds actions regarding viewing airports and flights
 class FlightsController < ApplicationController
   def index
+    @flights = Airport.all.map { |a| [a.airport_code, a.id]}
+    
     @available_flights = Flight
-    .where(from_airport: params[:flight_search][:from_airport_id])
-    .where(to_airport: params[:flight_search][:to_airport_id])
+    .where(from_airport: params[:flight_search][:from_airport_code])
+    .where(to_airport: params[:flight_search][:to_airport_code])
     .where(flight_date: params[:flight_search][:date_of_flight])
   end
 end
