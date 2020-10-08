7517480291e8687bebccaa1a5841e6bdbcf4cb04
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-08 10:24:41 -0700

    Update params to airport_id

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,8 +4,8 @@
 class FlightsController < ApplicationController
   def index
     @available_flights = Flight
-    .where(from_airport: params[:flight_search][:from_airport_code])
-    .where(to_airport: params[:flight_search][:to_airport_code])
+    .where(from_airport: params[:flight_search][:from_airport_id])
+    .where(to_airport: params[:flight_search][:to_airport_id])
     .where(flight_date: params[:flight_search][:date_of_flight])
   end
 end
