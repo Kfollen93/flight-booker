bb0e6558ac142d01d8e4f8bc7765dfa96613583b
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-10 17:49:58 -0700

    Change instance variable for readability and add if statement for params upon initial load

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,11 +3,13 @@
 # Holds actions regarding viewing airports and flights
 class FlightsController < ApplicationController
   def index
-    @flights = Airport.all.map { |a| [a.airport_code, a.id]}
-    
-    @available_flights = Flight
+    @airport_labels = Airport.all.map { |a| [a.airport_code, a.id]}
+
+    if params[:flight_search] 
+     @available_flights = Flight
     .where(from_airport: params[:flight_search][:from_airport_code])
     .where(to_airport: params[:flight_search][:to_airport_code])
     .where(flight_date: params[:flight_search][:date_of_flight])
+    end
   end
 end
