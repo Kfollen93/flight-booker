c0493d84d94b87148ae1cc918b9887d32afd3f00
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-13 13:18:48 -0700

    Create passenger variable

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,14 @@
 # Holds actions regarding bookings
 class BookingsController < ApplicationController
   def new
-    @selected_booking =  Flight
-    .where(from_airport: params[:booking][:from_airport_code])
-    .where(to_airport: params[:booking][:to_airport_code])
-    .where(flight_date: params[:booking][:date_of_flight])
+    @passenger = Passenger.new
+
+    # @selected_booking =  Flight
+    # .where(from_airport: params[:booking][:from_airport_code])
+    # .where(to_airport: params[:booking][:to_airport_code])
+    # .where(flight_date: params[:booking][:date_of_flight])
+    # .where(number_of_passengers: params[:booking][:passengers])
+
+    @selected_booking = Passenger.new
     end
 end
