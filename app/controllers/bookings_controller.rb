bf08c385412ba9e695a8350d8a559ffde5314cf9
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-16 11:44:38 -0700

    Format spacing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,7 @@ class BookingsController < ApplicationController
   def new
     @passenger = Passenger.new
     @booking = Booking.new
+
     @flight = Flight.find(params[:booking][:flight_id])
     @selected_booking = @flight.bookings.build
     
