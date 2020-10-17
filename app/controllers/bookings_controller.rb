e3d2cb300c098ba1f9eb3f0c544e7c1f48b23eb9
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-17 16:35:47 -0700

    Add create method and booking params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,24 @@ class BookingsController < ApplicationController
     @passenger = Passenger.new
     @booking = Booking.new
 
+    # @num_of_passengers = params[:booking][:passengers].to_i.times { Passenger.new }
+
     @flight = Flight.find(params[:booking][:flight_id])
     @selected_booking = @flight.bookings.build
-    
+  end
+  
+  def create
+    @selected_booking = @flight.bookings.build(booking_params)
+    if @selected_booking.save
+      flash[:notice] = "Booking was successfully created."
+      redirect_to booking_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def booking_params
+  #  params.require(:booking).permit(passengers_attributes: [:name, :email])
   end
 end
