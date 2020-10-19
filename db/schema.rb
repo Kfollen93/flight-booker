0593653c76972fb18ef2830fda12e86f286cd3dd
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-19 13:20:08 -0700

    Add passenger_id migration to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_160035) do
+ActiveRecord::Schema.define(version: 2020_10_19_201217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_160035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "flight_id"
+    t.integer "passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
