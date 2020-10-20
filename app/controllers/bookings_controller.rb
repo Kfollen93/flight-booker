ec67c78911020f66d78bb3282fb82cf3f3784f52
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-19 17:37:31 -0700

    Add number of passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,18 +3,17 @@
 # Holds actions regarding bookings
 class BookingsController < ApplicationController
   def new
-    @passenger = Passenger.new
-     2.times { @passenger.bookings.build }
     @booking = Booking.new
 
+    num_of_passengers = params[:booking][:passengers]
+    num_of_passengers.to_i.times { @booking.build_passenger }
+  
     @flight = Flight.find(params[:booking][:flight_id])
     @selected_booking = @flight.bookings.build
   end
   
   def create
-    @flight = Flight.new(flight_params)
     @booking = Booking.new(booking_params)
-    @booking.flight = @flight
     
     if @booking.save
       flash[:notice] = "Booking was successfully created."
@@ -25,14 +24,11 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   private
   def booking_params
     params.require(:booking).permit(:flight_id, passenger_attributes: [:name, :email])
-  end
-
-  def flight_params
-    params.require(:booking).permit(:flight_id)
   end
 end
