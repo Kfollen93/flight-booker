68393d90a39e88a16d043a394790027d2ad3706e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-20 10:00:02 -0700

    Create add column migration for booking_id and accept nested attributes for many passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,13 +3,15 @@
 # Holds actions regarding bookings
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new
-
-    num_of_passengers = params[:booking][:passengers]
-    num_of_passengers.to_i.times { @booking.build_passenger }
-  
     @flight = Flight.find(params[:booking][:flight_id])
+    
     @selected_booking = @flight.bookings.build
+    @booking = Booking.new
+    num_of_passengers = params[:booking][:passengers]
+    num_of_passengers.to_i.times { @booking.passengers.build }
+  
+
+    
   end
   
   def create
@@ -17,18 +19,18 @@ class BookingsController < ApplicationController
     
     if @booking.save
       flash[:notice] = "Booking was successfully created."
-      redirect_to booking_path(@booking)
+      redirect_to @booking
     else
       render :new
     end
   end
 
   def show
-    @booking = Booking.find(params[:id])
+    @booking = Booking.find(params[:booking][:flight_id])
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:flight_id, passenger_attributes: [:name, :email])
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_201217) do
+ActiveRecord::Schema.define(version: 2020_10_20_154506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_201217) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
