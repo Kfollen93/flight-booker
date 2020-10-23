6d447ad7a04c53b9447e4f1d1aa42f05ce146963
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-22 17:12:06 -0700

    Add name and email validations

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,6 @@
 class Passenger < ApplicationRecord
-  validates :name, :email, presence: true
+  validates :name, :email, presence: true, length: { maximum: 60 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   belongs_to :booking
   has_many :flights, through: :bookings
